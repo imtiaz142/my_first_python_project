2ec8b2c27d36f87797628c96e9fbe654b60c3697
imtiaz-epazz <imtiaz.epazz@gmail.com>
2025-06-17 02:50:25 -0700

    my midule aded

--- main.py
+++ main.py
@@ -2,3 +2,13 @@ from helper import greet
 
 print("Starting program with git...")
 greet("Imtiaz Ali")
+
+
+import MyModules
+ 
+from MyModules import add, subtract, multiple
+
+print(MyModules.add(2,8))
+
+print(subtract(3,5))
+
