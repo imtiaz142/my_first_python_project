1aa0b8b1544a4621acaae1470a2530cc7e0589c5
imtiaz142 <imtiaz8182@gmail.com>
2025-07-01 02:09:51 -0700

    Data type is added

---
+++ 02_data_types.py
@@ -0,0 +1,47 @@
+
+# #  -------------------Integer (int)-------------------------------
+
+a : int = 16
+b : int = 4
+
+print(a//b)  #reminder ni data means point k bd wali value 
+print(a % b)  # 0 remainder dta ha 
+# print(divmod(9,4)) #(2,1) 9 // 4 → 2, 9 % 4 → 1
+
+# print(abs(-50))  #(50) abs() function means absolute value
+
+# print(bin(10)) #(0b10101)
+
+# int("123") #String into int
+
+# x = int(7.9)
+# print(x) #7
+
+# import math
+
+# print(math.isqrt(10))
+
+# print(2**3) #8
+
+
+#---------------------------Floating-Point---------------------
+
+# num_float: float = 3.14
+# #num_float: float = .14
+
+# print(type(num_float), " num_float = ", num_float)  # 
+
+
+
+#----------------------------Complex (complex)---------------------
+
+
+z = 3 + 4j
+
+print(z.real) # 3.0
+print(z.imag)  # 4.0
+print(abs(z))  # 5.0 (9+16)=25=5.0
+
+
+#----------------------------String---------------------
+
