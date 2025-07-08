ea93406419facb69cb124a859708055de964d304
imtiaz142 <imtiaz8182@gmail.com>
2025-07-08 01:42:03 -0700

    sTRING CASTING IS ADDED

---
+++ 04_strings_casting.py
@@ -0,0 +1,16 @@
+a =   'This is single quote string'
+b= "this is double quote string"
+c= """" This is 
+    multiline string"""
+
+c="C:\\Users\\Ali"
+
+
+print("\u0041 = ", "\u0041")
+
+text = "one,two,three,four,five,six,seven,eight,nine"
+print(text.split(",", 1))
+
+text = "Python is awesome"
+print(text.find("is"))  # 7
+print(text.find("Java"))  # -1
